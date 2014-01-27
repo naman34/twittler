ed803e765cbef91213f6de2dc304e7d1a153a0a1
Naman Goel <naman34@gmail.com>
2014-01-28 03:36:28 +0530

    Completed all the basic Requirements of the project. Also included timestamps in the time ago format. So now, I only have to let the user tweet to finish all the requirements of extra credit.

---
+++ js/main.js
@@ -0,0 +1,92 @@
+//A self-executing function is usually more performanat that document.ready
+(function(window, $, undefined){
+
+
+
+	var $tweets = $('.tweets');
+
+	function loadTweets(){
+		for(var i = $('.tweets .tweet').length; i < streams.home.length; i++){
+			$tweets.prepend(tweetTemplate(streams.home[i]));
+			
+			//converts timestamps into human-readable time-ago format, and autoupdates.
+			$($('.tweet')[0]).find('.time').timeago();
+
+			//making sure the timeline doesn't keep scrolling by at the amzing intense speed.
+			if($('.tweet').length > 5){
+				$('.tweets').scrollTop($('.tweets').scrollTop() + 73 + $($('.tweet')[0]).height());	
+			}
+
+		}
+	}
+
+	function loadUserTweets(){
+
+		var username = window.location.hash.replace('#/', "").replace('#', "");
+
+		if(!!streams.users[username]){
+			for(var i = $('.tweets .tweet').length; i < streams.users[username].length; i++){
+				$tweets.prepend(tweetTemplate(streams.users[username][i]));
+				
+				//converts timestamps into human-readable time-ago format, and autoupdates.
+				$($('.tweet')[0]).find('.time').timeago();
+
+				//making sure the timeline doesn't keep scrolling by at the amzing intense speed.
+				if($('.tweet').length > 5){
+					$('.tweets').scrollTop($('.tweets').scrollTop() + 73 + $($('.tweet')[0]).height());	
+				}
+
+			}
+		} else {
+			alert("The user doesn't exist. 404!");
+			window.location.hash = "";
+		}
+
+		
+	}
+
+	var autoloader;
+
+	function router(){
+
+		if(!!autoloader){
+			clearInterval(autoloader);
+		}
+		$('.tweets').html("");
+
+		if(window.location.hash === "" || window.location.hash === "#" || window.location.hash === "#/" ){
+		
+		autoloader = setInterval(loadTweets, 1000);
+	
+		} else if(!!window.location.hash.match(/#(\/)?[a-z0-9]+/i)) {
+
+			autoloader = setInterval(loadUserTweets, 1000);
+
+		} else {
+
+			alert("Invalid Address, redirecting to home.");
+			window.location.hash = "";
+
+		}
+	}
+
+	router();
+
+	window.addEventListener("hashchange", router, false);
+	
+	
+	function tweetTemplate(tweet){
+		if(!tweet.user || !tweet.message){
+			return "";
+		}
+
+		var string = '<div class="tweet">';
+		string += '<span class="time" title="' + (tweet.created_at ? (new Date(tweet.created_at)).toJSON() : (new Date()).toJSON() ) + '" ></span>';
+		string += '<h3><a href="#/' + tweet.user + '" > @' + tweet.user + '</a></h3>';
+		string += tweet.message.replace(/#[a-z0-9]{1,}/, "<strong class='hashtag'>" + tweet.message.match(/#[a-z0-9]{1,}/i) + "</strong>");
+		string += '</div>';
+
+		return string;
+	}
+
+})(this, $)
